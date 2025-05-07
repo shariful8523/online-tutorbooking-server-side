6c8851445ac9bb04628b6046939af8f50dca753e
shariful8523 <mdsharifulislam8523@gmail.com>
2025-05-08 04:52:50 +0600

    server side to recive data and send database

--- index.js
+++ index.js
@@ -21,37 +21,47 @@ const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@clu
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-     const  database = client.db("TutorsDB");
-     const TutorCollection = database.collection("Tutors")
+        const database = client.db("TutorsDB");
+        const TutorCollection = database.collection("Tutors")
 
-    // Tutors related apis 
+        // Tutors related apis 
 
-    
+        app.get('/tutors', async (req, res) => {
+            const cursor = TutorCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.post('/tutors', async (req, res) => {
+            const tutors = req.body;
+            const result = await TutorCollection.insertOne(tutors);
+            res.send(result)
+        })
 
 
 
 
 
 
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
