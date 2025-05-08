59b469d45c66a23e19f56277357983932e1b35ee
shariful8523 <mdsharifulislam8523@gmail.com>
2025-05-09 03:22:31 +0600

    get tutors find by email

--- index.js
+++ index.js
@@ -5,21 +5,14 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-
-
 // middle ware 
-
 app.use(cors({
     origin: '*',
 }));
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.wfpeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -30,58 +23,63 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         const database = client.db("TutorsDB");
         const TutorCollection = database.collection("Tutors")
 
-        // Tutors related apis 
-
+        // All tutors
         app.get('/tutors', async (req, res) => {
             const cursor = TutorCollection.find();
             const result = await cursor.toArray();
             res.send(result)
         })
 
+        // Add a tutor
         app.post('/tutors', async (req, res) => {
             const tutors = req.body;
             const result = await TutorCollection.insertOne(tutors);
             res.send(result)
         })
 
+        // Get Tutors find filter by  userGmail
 
-        // Single language tutors route
-        app.get('/tutors/:language', async (req, res) => {
+        app.get('/tutors', async (req, res) => {
+           const {userEmail} = req.query;
+           let query = {} ;
+
+           if (userEmail) {
+            query.userEmail= userEmail;
+           }
+
+           const cursor = TutorCollection.find(query);
+           const result = await cursor.toArray();
+           res.send(result);
+        });
+
+        //  Single tutor by ID
+        app.get('/tutor/:id', async (req, res) => {  // Route pattern change
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const tutor = await TutorCollection.findOne(query);
+            res.send(tutor)
+        })
+
+        // Tutors by language
+        app.get('/tutors/language/:language', async (req, res) => {
             const language = req.params.language;
-            const query = { language: language };  
-            const tutors = await TutorCollection.find(query).toArray();  
+            const query = { language: language };
+            const tutors = await TutorCollection.find(query).toArray();
             res.send(tutors);
         });
-        
-        
-
-
-
-
 
     } finally {
-        // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
-
 
 app.get('/', (req, res) => {
     res.send('server is running')
