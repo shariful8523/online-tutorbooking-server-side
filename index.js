ffddf9ab7e6a7dd566a77e5b1e7cb5a2b093515e
shariful8523 <mdsharifulislam8523@gmail.com>
2025-05-15 01:00:58 +0600

    added jwt authentication and verify token

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
+    credentials: true
 }));
 app.use(express.json());
+app.use(cookieParser());
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token;
+     
+    if(!token) {
+        return res.status(401).send({ message: 'unauthorized access'});
+    }
+
+    // verify the token
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) {
+            return res.status(401).send({message: 'unauthorized access'});
+        }
+      
+        next();
+    })
+
+    
+}
+
+
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.wfpeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -30,12 +57,43 @@ async function run() {
         const database = client.db("TutorsDB");
         const TutorCollection = database.collection("Tutors");
 
+        // jwt auth relented apis
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            });
+
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false
+                })
+                .send({ success: true })
+        })
+
+        app.post('/logout', (req, res) => {
+            res 
+            .clearCookie('token',{
+                httpOnly:true,
+                secure: false
+            })
+            .send({success: true})
+        })
+
+
+
+
+
+
+
         // Book a Tutorials
 
         const BookingCollection = database.collection("Bookings");
 
         // Get all tutors or filter by userEmail
-        app.get('/tutor', async (req, res) => {
+        app.get('/tutor', verifyToken, async (req, res) => {
             const { userEmail } = req.query;
             let query = {};
 
@@ -56,7 +114,7 @@ async function run() {
         });
 
         // Get single tutor by ID
-        app.get('/tutor/:id', async (req, res) => {
+        app.get('/tutor/:id', verifyToken, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await TutorCollection.findOne(query);
@@ -64,7 +122,7 @@ async function run() {
         });
 
         // Get tutors by language
-        app.get('/tutor/language/:language', async (req, res) => {
+        app.get('/tutor/language/:language', verifyToken, async (req, res) => {
             const language = req.params.language;
             const query = { language: language };
             const tutors = await TutorCollection.find(query).toArray();
