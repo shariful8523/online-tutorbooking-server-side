6a447689770f1f73578204275970d325ff677ebc
Md shariful islam <158291469+shariful8523@users.noreply.github.com>
2025-10-26 03:44:09 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://online-tutorbooking-server-side-5ns.vercel.app',
     credentials: true
 }));
 app.use(express.json());
